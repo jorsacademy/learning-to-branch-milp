336d37b36eedd2c5d7bbf21f724b2ebbca432aec
Jors Academy <abalci16@posta.pau.edu.tr>
2026-08-29 22:58:35 +0300

    fix: organize exports and expose tree dataset collector

--- src/ltb_milp/__init__.py
+++ src/ltb_milp/__init__.py
@@ -1,11 +1,25 @@
 """Learning-to-branch utilities for small binary MILPs."""
 
-from ltb_milp.branching import BranchDecision, most_fractional, strong_branch, strong_branch_scores
-from ltb_milp.dataset import BranchingDataset, collect_root_strong_branching_dataset
+from ltb_milp.branching import (
+    BranchDecision,
+    most_fractional,
+    strong_branch,
+    strong_branch_scores,
+)
+from ltb_milp.dataset import (
+    BranchingDataset,
+    collect_root_strong_branching_dataset,
+    collect_tree_strong_branching_dataset,
+)
 from ltb_milp.features import candidate_features
 from ltb_milp.models import BranchingMLP
 from ltb_milp.policies import learned_branch_policy
-from ltb_milp.problem import BinaryPackingMILP, LPSolution, generate_binary_packing, solve_lp_relaxation
+from ltb_milp.problem import (
+    BinaryPackingMILP,
+    LPSolution,
+    generate_binary_packing,
+    solve_lp_relaxation,
+)
 from ltb_milp.solver import SolveResult, solve_branch_and_bound
 from ltb_milp.training import predict_scores, train_branching_model
 
@@ -18,6 +32,7 @@ __all__ = [
     "SolveResult",
     "candidate_features",
     "collect_root_strong_branching_dataset",
+    "collect_tree_strong_branching_dataset",
     "generate_binary_packing",
     "learned_branch_policy",
     "most_fractional",
